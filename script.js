89251d14a978ddba545bfc6bb8c052df724f0c6e
Nitesh Coder <152203678+Coderniteshkumar@users.noreply.github.com>
2024-08-11 22:02:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+
+// var n="nitesh";
+// n="kumar";
+// console.log(n)
+// output is kumar
+
+// let n="nitesh";
+// n="kumar";
+// console.log(n)
+// output is kumar
+
+// const n="nitesh";
+// n="kumar";
+// console.log(n)
+// output type error
+
+
+// function main(n){
+//     if(n){
+//         var name="nitesh"
+//         // var sname="kumar"
+//         //  let name="nitesh"
+//          const sname="kumar"
+//         console.log("my name is :"+name+" "+sname);
+//     }
+// }
+
+// main(true)
+
+// var nam="india"
+// if(true){
+
+//     var nam="bihar"
+//     console.log(nam);
+
+// }
+// console.log(nam);
+
+// var name="india"
+// if(true){
+
+//     let name="bihar"
+//     console.log(name);
+
+// }
+// console.log(name);
+
+// const name="india"
+// if(true){
+
+//     const name="bihar"
+//     console.log(name);
+
+// }
+// console.log(name);
+
+// let fname="nitesh"
+// let lname="kumar"
+
+// console.log(`my name is ${fname} ${lname} `);
+
+// let a=20
+// let b=30
+// console.log(`fifty is ${a+b} 
+//      and not ${2*a+b}`);
+
+let fname=" nitesh"
+// console.log(`${fname}`.startsWith('n'));
+// console.log(`${fname}`.includes('e'));
+// console.log(`${fname}`.endsWith('h'));
+
+// console.log((fname) .repeat(10))
+
+
+// let list=['java','python','javascript']
+// console.log(`${list}`);
+// console.log(list[1][2]);
+// var t1=list[0]
+// var t2=list[1]
+// var t3=list[2]
+// console.log(`my first program is:${list[0]} \and my second program is ${list[1]}`);
+// console.log(list.length);
+
+// let a=5
+// let b=10
+// let temp=a
+// a=b
+// b=temp
+// console.log(`a value is ${a} b is value is ${b}`);
+
+// const data= {
+//     name:"nitesh",
+//     age:21,
+//     deg:'bca',
+//     skill:{
+//         first:"node js",
+//         sec:"react js"
+//     }
+
+// }
+// let{name,age,deg,skill}=data
+// console.log(`my name is ${name} my age is ${age} my deg is ${deg} my skill is ${skill.first}`);
+
+// ES5
+
+// fat arrow function
+// var sum=function(){
+//     var a=10
+//     var b=20
+//     return a+b
+// }
+// console.log(sum());
+
+// ES6
+
+// const sum=()=>{
+//     let a=20
+//     let b=40
+//     return a+b
+// }
+// console.log(sum());
+
+
+// let a=20
+// let b=45
+// const sum=()=> a+b
+// console.log(sum());
+
+// let a=30
+// const sum=(b)=> a+b
+// console.log(sum(20));
+
+// function mult(a,b=4){
+//     return(a*b);
+//     console.log(`mult is ${a*b}`);
+// }
+
+// console.log(mult(4));
+
+
+// rest parameter
+// ...[ths is rest opeater]
+// function sum(...data){
+// console.log(data);
+// console.log(...data);
+
+// let total=0
+// for(let i of data){
+//     total=total+i
+// }
+// console.log(total);
+
+// }
+// sum(1,2,3,4,5,6,)
+
+// sepreat oprater
+// function sum(a,b,c){
+//     console.log(a+b+c);
+// }
+// var arr=[10,2,5]
+// sum(...arr)
+
+// concat
+// let arr1=[1,2,3]
+// let arr2=[3,4,5]
+// let arr3=[6,7,8]
+// data=arr1.concat(arr2)
+// data=[...arr1,...arr2,...arr3]
+// console.log(data);
+
+// example
+
+// var arr1=[1,2,3]
+// var arr2=[4,5,6]
+// var arr3=[7,8,9]
+//  totalarr=arr1.concat(arr2).concat(arr3)
+//  console.log(totalarr);
+ 
+ 
+
+// let arrc1=[1,2,3]
+// let arrc3=[6,7,8]
+// let arrc2=arrc1
+// arrc2.push(4,5)
+
+// arrc2=[...arrc1,4,5,...arrc3]
+// console.log(arrc2);
+
+// Math.sign is a define possitive and negative number
+// let number=5.5
+// console.log(Math.sign(number));
+
+// Math.trunc is a define be integer number before point
+// let number=5.5558798
+// console.log(Math.trunc(number));
+
+// var n=5.10
+// console.log(Math.trunc(n));
+
+
+// power
+// let a=5; let b=3
+// console.log(a**b);
+
+// let n=25
+// console.log(Math.sqrt(n));
+
+// let a=10
+// console.log(a**4);
+
+// let num=5
+// console.log(Number.isInteger(num));
+
+// let n=[5,4,3,2,1,6,7,8,9,10]
+// var sum=0
+// for(let i=1;i<=n.length;i++){
+//  sum=sum+i
+// }
+// console.log("sum=",sum);
+
+
+
+
+
